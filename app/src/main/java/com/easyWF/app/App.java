4463b0bfe54a6f34065a027a8d4fa9d6eb95b545
“cb-aftab” <aftab@chargebee.com>
2017-10-25 15:39:21 +0530

    Adding pom.xml

---
+++ app/src/main/java/com/easyWF/app/App.java
@@ -0,0 +1,13 @@
+package com.easyWF.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
